b26b723d2f4d080efe1027ddbd9d22195c4bcede
Lucas Marchiori <sinonsrt@hotmail.com>
2025-06-30 19:39:28 -0300

    feat: configure template and styles

--- gallery/urls.py
+++ gallery/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
 
-from gallery.views import index
+from gallery.views import image, index
 
-urlpatterns = [path("", index)]
+urlpatterns = [path("", index, name="index"), path("image/", image, name="image")]
--- gallery/views.py
+++ gallery/views.py
@@ -2,4 +2,8 @@ from django.shortcuts import render
 
 
 def index(request):
-    return render(request, "index.html")
+    return render(request, "gallery/index.html")
+
+
+def image(request):
+    return render(request, "gallery/image.html")
